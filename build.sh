c1bdba1c48fffe72eb3b987a5372f999aa5c1267
Craig Jones <craig.jones@firmus.co>
2025-04-04 11:46:33 +1100

    BAK-1015 Source version from VERSION file Handle building from source code only

--- build.sh
+++ build.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <version>"
-    exit 1
-fi
-
-VERSION=$1
+VERSION=$(cat VERSION)
 
 PLATFORMS=("linux/amd64")
 
 BUILD_DATE=$(date --iso-8601=m --utc)
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-GIT_REVISION=$(git rev-parse HEAD)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 
 LDFLAGS="-X github.com/prometheus/common/version.BuildUser=$USER
          -X github.com/prometheus/common/version.BuildDate=$BUILD_DATE
          -X github.com/prometheus/common/version.Version=$VERSION 
-         -X github.com/prometheus/common/version.Branch=$GIT_BRANCH
-         -X github.com/prometheus/common/version.Revision=$GIT_REVISION"
+         -X github.com/prometheus/common/version.Branch=$GIT_BRANCH"
 
 for platform in "${PLATFORMS[@]}"
 do
@@ -25,6 +18,7 @@ do
     GOOS=${split[0]} 
     GOARCH=${split[1]}
     OUTPUT_PATH="build/smc-exporter-${VERSION}-${GOOS}-${GOARCH}"
+    echo "Building ${OUTPUT_PATH}..."
     CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -o "${OUTPUT_PATH}" -ldflags "$LDFLAGS"
     chmod +x "${OUTPUT_PATH}"
 done
